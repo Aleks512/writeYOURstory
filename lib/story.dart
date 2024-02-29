ce7aed5caf5209db66759a7229d27b7943b6d06c
Aleks512 <aleksandracichocka999@gmail.com>
2024-02-29 16:03:37 +0100

    comments and decoration

---
+++ lib/story.dart
@@ -0,0 +1,12 @@
+//TODO: Step 2 done - Create a new class called Story.
+class Story {
+  String? storyTitle;
+  String? choice1;
+  String? choice2;
+
+  Story(this.storyTitle, this.choice1, this.choice2);
+}
+
+//TODO: Step 3 done - Create 3 properties for this class, A. C to store the story text. B. C to store the text for choice 1, C. C to store the text for choice 2.
+
+//TODO: Step 4 done - Create a Constructor for this class to be able to initialise the properties created in step 3.
